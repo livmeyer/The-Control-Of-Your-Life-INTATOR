6f5ba04d75f4eff2121e47ad942f70b286650c0c
Liv <olivia-Meyer-02@outlook.com>
2024-02-18 11:04:31 +0100

    Added Activity made minor Changes

--- app/src/main/java/com/example/thecontroll_of_your_life_inator/MainActivity.kt
+++ app/src/main/java/com/example/thecontroll_of_your_life_inator/MainActivity.kt
@@ -1,43 +1,29 @@
 package com.example.thecontroll_of_your_life_inator
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.thecontroll_of_your_life_inator.ui.theme.TheControllOfYourLifeINATORTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TheControllOfYourLifeINATORTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TheControllOfYourLifeINATORTheme {
-        Greeting("Android")
+        AndroidView(
+            factory = { context ->
+                LayoutInflater.from(context).inflate(R.layout.activity_main, null)
+            },
+            update = {}
+        )
     }
 }
