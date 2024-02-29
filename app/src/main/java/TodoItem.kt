b456cb24e8503d4fe6337e994a6abf90adff1c10
Liv <olivia-Meyer-02@outlook.com>
2024-02-29 10:35:06 +0100

    Small cosmetic Changes, Todoitem Class

---
+++ app/src/main/java/TodoItem.kt
@@ -0,0 +1,9 @@
+import java.util.Date
+
+class TodoItem(name: String){
+    val name: String? = null
+    val lastCompleted: Date? = null
+    var daily = false
+    var appointment: Date? = null
+    var notes: String? = null
+}
